b0045f64c024c70d887780eb2261f9aa900048ea
rohan <rohanlokhande2022@gmail.com>
2026-06-09 23:58:46 +0530

    SingleThreadExecutor use case

--- MultithreadingConcepts/src/CoreMultithreadingConcepts/ExecutorServiceDemo/SingleThreadedExecutorDemo.java
+++ MultithreadingConcepts/src/CoreMultithreadingConcepts/ExecutorServiceDemo/SingleThreadedExecutorDemo.java
@@ -4,6 +4,12 @@ import java.awt.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+// Executor service is an interface is used to manage the thread pool using this
+// we don't need to start the thrad and manage
+// Here we have created the SingleThradedExecutor where only single thread will run
+// and complete the task in the sequential order. But here if the thead is dead or stopped then
+// it executor will create the new thread automatically
+
 public class SingleThreadedExecutorDemo {
     public static void main(String[] args) {
         try(ExecutorService service = Executors.newSingleThreadExecutor()){
